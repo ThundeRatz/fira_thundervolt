8670dfc878515e99d76e7f11f385b2cb8203cc5f
lucastrschneider <lucastrschneider@gmail.com>
2021-02-16 16:38:38 -0300

    🐛 Fix initialization bug

--- thundervolt/core/data.py
+++ thundervolt/core/data.py
@@ -6,9 +6,9 @@ GOAL_DEPTH = 0.1
 
 class Pose2D:
     def __init__(self, x: float = 0, y: float = 0, theta: float = 0):
-        self.x = 0
-        self.y = 0
-        self.theta = 0
+        self.x = x
+        self.y = y
+        self.theta = theta
 
     def __str__(self):
         return f'x: {self.x:.02f}  y: {self.y:.02f}  th: {self.theta:.02f}'
