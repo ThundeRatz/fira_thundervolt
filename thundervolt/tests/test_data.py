fa85d49509880fffd49578d8d5f66d51196d0834
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2021-02-17 17:49:25 -0300

    Add data creation tests

--- thundervolt/tests/test_data.py
+++ thundervolt/tests/test_data.py
@@ -1,11 +1,22 @@
-# import test_base  # pylint: disable=import-error
-
 import pytest
-from thundervolt.core.data import EntityData
+from thundervolt.core.data import Pose2D, EntityData, FieldData
+
+
+def test_creation_pose2d():
+    data = Pose2D()
+    assert data.x == pytest.approx(0.0)
+    assert data.y == pytest.approx(0.0)
+    assert data.theta == pytest.approx(0.0)
 
 
 def test_creation_entity():
     data = EntityData()
-    assert data.position.x == pytest.approx(0.0)
-    assert data.position.y == pytest.approx(0.0)
-    assert data.position.theta == pytest.approx(0.0)
+    assert type(data.position) is Pose2D
+    assert type(data.velocity) is Pose2D
+
+
+def test_creation_field_data():
+    data = FieldData()
+    assert type(data.robots[0]) is EntityData
+    assert type(data.foes[0]) is EntityData
+    assert type(data.ball) is EntityData
