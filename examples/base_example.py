10565f0d175c7d71be1acbac6181d3eb548d964d
lucastrschneider <lucastrschneider@gmail.com>
2021-02-18 21:01:56 -0300

    🚚 Move examples folder

--- examples/base_example.py
+++ examples/base_example.py
@@ -3,4 +3,4 @@
 import sys
 import pathlib
 
-sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.parent.absolute()))
+sys.path.append(str(pathlib.Path(__file__).parent.parent.absolute()))
