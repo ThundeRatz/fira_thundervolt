e25f698b8480502ba0b0ca9fcd54fc7c09e4106b
lucastrschneider <lucastrschneider@gmail.com>
2021-02-20 20:30:46 -0300

    Add saturation to look at action - Remove unused class member

--- thundervolt/actions/look_at_action.py
+++ thundervolt/actions/look_at_action.py
@@ -12,20 +12,20 @@ class LookAtAction(Action):
         self.tolerance = tolerance
         self.controller = pidController(kp, ki, kd)
         self.controller.set_point = 0.0
+        self.controller.saturation = kp * np.pi
 
     def initialize(self, robot_id):
         super().initialize(robot_id)
-        self.last_received_angle = None
         self.controller.reset()
 
     def set_angle(self, angle):
         self.final_angle = assert_angle(angle)
 
     def update(self, field_data: FieldData) -> (RobotCommand, bool):
-        self.last_received_angle = field_data.robots[self.robot_id].position.theta
+        last_received_angle = field_data.robots[self.robot_id].position.theta
 
         # Check if it is inside the tolerance
-        if abs(self.final_angle - self.last_received_angle) < self.tolerance:
+        if abs(assert_angle(self.final_angle - last_received_angle)) < self.tolerance:
             return (RobotCommand(), True)
 
         # Calculate controller response
