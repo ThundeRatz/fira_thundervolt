ac1322502032a30ed0b7c5fa95503c47f2e8e01f
LucasHaug <lucas.haug@hotmail.com>
2021-02-20 20:44:15 -0300

    :bug: Fix line action error bug

--- thundervolt/actions/line_action.py
+++ thundervolt/actions/line_action.py
@@ -69,7 +69,7 @@ class LineAction(Action):
 
         response_lin *= np.cos(assert_angle(goal_ang - actual_ang))**2
 
-        distance_to_goal = actual_ang - goal_ang
-        response_ang = self.controller_ang.update(distance_to_goal)
+        angle_to_goal = assert_angle(actual_ang - goal_ang)
+        response_ang = self.controller_ang.update(angle_to_goal)
 
         return (RobotCommand(response_lin - response_ang, response_lin + response_ang), False)
