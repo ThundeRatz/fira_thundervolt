96a3d327951b1856f995e3c89d8c0c47e83a778a
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2021-02-20 16:02:38 -0300

    🚧Update line_action

--- thundervolt/actions/line_action.py
+++ thundervolt/actions/line_action.py
@@ -1,30 +1,62 @@
 import numpy as np
 
 from .action import Action
+from thundervolt.core.pid_controller import pidController
 from thundervolt.core.data import FieldData, Pose2D
 from thundervolt.core.command import RobotCommand
 
 class LineAction(Action):
-    def __init__(self, kp, ki, kd, tolerance):
-        super().__init__(kp, ki, kd, tolerance)
+    def __init__(self, kp_lin, ki_lin, kd_lin, tolerance_lin, kp_ang, ki_ang, kd_ang, tolerance_ang):
+        super().__init__()
+        self.tolerance_lin = tolerance_lin
+        self.controller_lin = pidController(kp_lin, ki_lin, kd_lin)
+        self.tolerance_ang = tolerance_ang
+        self.controller_ang = pidController(kp_ang, ki_ang, kd_ang)
 
-    def initialize(self, robot_id, pointA, pointB, pointBall):
+
+    def initialize(self, robot_id, pointA, pointB):
         super().initialize(robot_id)
-        goal = Pose2D()
+        self.controller_lin.reset()
+        self.controller_ang.reset()
 
-        if pointB.x < pointA.x:
+        if pointB[0] < pointA[0]:
             pointA, pointB = pointB, pointA
 
-        aux = (pointB.y - pointA.y) / (pointB.x - pointA.x)
-        goal.x = (aux * (aux*pointA.x - pointA.y + pointBall.y) + pointBall.x) / (1 + aux**2)
-        goal.y = (aux * (aux*pointB.y - pointA.x + pointBall.x) + pointB.x) / (1 + aux**2)
+        self.pointA = pointA
+        self.pointB = pointB
+        self.aux = (pointB[1] - pointA[1]) / (pointB[0] - pointA[0])
 
-        if goal.x < pointA.x:
-            goal = pointA
-        elif goal.x > pointB.x:
-            goal = pointB
+        self.controller_lin.set_point(0)
+
+    def set_goal(self, pointC):
+        u = pointC - self.pointA
+        v = self.pointB - self.pointA
+        v_versor = utils.versor(v)
+        u_dot_v = np.dot(u, v_versor)
+
+        if u_dot_v < 0:
+            goal = self.pointA
+        elif u_dot_v > np.linalg.norm(v):
+            goal = self.pointB
+        else:
+            goal = self.pointA + v_versor*u_dot_v
 
         self.goal = goal
 
     def update(self, field_data: FieldData) -> RobotCommand:
-        return
+        actual_point = np.zeros(2)
+
+        actual_point[0] = field_data.robots[self.robot_id].position.x
+        actual_point[1] = field_data.robots[self.robot_id].position.y
+        actual_ang = field_data.robots[self.robot_id].position.theta
+
+        if np.linalg.norm(self.goal-actual_point) < self.tolerance:
+            return (RobotCommand(), True)
+
+        goal_ang = np.arctan2(actual_point-self.goal)
+        self.controller_ang.set_point(goal_ang)
+
+        response_ang = self.controller.update(actual_ang)
+        response_lin = self.controller_lin.update(np.linalg.norm(self.goal-actual_point))
+
+        return (RobotCommand(response_lin - response_ang, response_lin + response_ang), False)
