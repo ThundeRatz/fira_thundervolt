f48afdc02934a8a2b61001600fab11c722ba11e3
LucasHaug <lucas.haug@hotmail.com>
2021-02-20 21:29:59 -0300

    :wrench: Add config file option

--- thundervolt/__main__.py
+++ thundervolt/__main__.py
@@ -1,14 +1,29 @@
 import thundervolt
 import logging
+import argparse
+import json
 
-if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser(description='ThunderVolt')
+    parser.add_argument('--config_file', default='config.json')
+
+    args = parser.parse_args()
+    config_file = args.config_file
+
+    config = json.loads(open(config_file, 'r').read())
+
+    print(config)
 
     logging.basicConfig(filename='logfile.log',
                         level=logging.DEBUG)
 
-    my_game = thundervolt.game.Game()
+    my_game = thundervolt.game.Game(config)
 
     try:
         my_game.run()
     except KeyboardInterrupt:
         logging.warn("Ending")
+
+
+if __name__ == '__main__':
+    main()
--- thundervolt/game.py
+++ thundervolt/game.py
@@ -1,7 +1,13 @@
+from .comm.vision import FiraVision
+from .comm.control import FiraControl
+from .comm.referee import RefereeComm
+from .core.data import FieldData
+from .core.command import TeamCommand
+
 # Faz toda a parte de comunicação e cria o Coach
 
 class Game():
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         print("Starting...")
 
     def run(self):
