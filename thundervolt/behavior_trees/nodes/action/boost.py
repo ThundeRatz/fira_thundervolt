3200a3c367050f014e0ee45751802add56542ec4
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2021-02-22 15:20:12 -0300

    ✅Add integration tests for Boost

--- thundervolt/behavior_trees/nodes/action/boost.py
+++ thundervolt/behavior_trees/nodes/action/boost.py
@@ -12,8 +12,8 @@ class BoostBall(ExecutionNode):
         self.team_command = team_command
 
     def setup(self):
-        self.action = FollowFieldAction(kp_ang=10.0, ki_ang=0.0, kd_ang=3.0, kp_lin=60.0, ki_lin=0.0, kd_lin=3.0, tolerance_lin=0.01,
-                                base_speed=20, goal=None, use_front=True, saturation_ang=None, saturation_lin=None)
+        self.action = FollowFieldAction(kp_ang=10.0, ki_ang=0.005, kd_ang=3.0, kp_lin=100.0, ki_lin=0.005, kd_lin=3.0, tolerance_lin=0.07,
+                                base_speed=20, goal=None, use_front=True, saturation_ang=None, saturation_lin=10.0)
 
     def initialise(self):
         ball_point = np.array([self.field_data.ball.position.x, self.field_data.ball.position.y])
