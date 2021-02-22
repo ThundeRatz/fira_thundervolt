60e6e9ada0e4a615efaaac0d14810b17da78baa8
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2021-02-22 01:07:24 -0300

    ✨Begin boost node action

---
+++ thundervolt/behavior_trees/nodes/action/boost.py
@@ -0,0 +1,36 @@
+import numpy as np
+import py_trees
+
+from ..execution_node import ExecutionNode
+from thundervolt.core import data
+from thundervolt.actions.follow_field_action import FollowFieldAction
+from thundervolt.vector_fields.fields import RadialField
+
+class BoostBall(ExecutionNode):
+    def __init__(self, name, role, field_data, team_command):
+        super().__init__(name, role, field_data)
+        self.team_command = team_command
+
+    def setup(self):
+        self.action = FollowFieldAction(kp_ang=10.0, ki_ang=0.0, kd_ang=3.0, kp_lin=60.0, ki_lin=0.0, kd_lin=3.0, tolerance_lin=0.01,
+                                base_speed=20, goal=None, use_front=True)
+
+    def initialise(self):
+        self.vector_field = RadialField(target=(self.field_data.ball.position.x, self.field_data.ball.position.y))
+        self.action.initialize(self.parameters.robot_id, self.vector_field)
+
+    def update(self):
+        ball_point = np.array([self.field_data.ball.position.x, self.field_data.ball.position.y])
+        self.vector_field.target = ball_point
+        self.action.set_goal(ball_point)
+
+        robot_cmd, action_state = self.action.update(self.field_data)
+        self.team_command.commands[self.parameters.robot_id] = robot_cmd
+
+        if action_state == True:
+            return py_trees.common.Status.SUCCESS
+        else:
+            return py_trees.common.Status.RUNNING
+
+    def terminate(self, new_status):
+        pass
