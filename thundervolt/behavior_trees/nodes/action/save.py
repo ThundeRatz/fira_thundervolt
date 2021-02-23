91686447bc6396f73a27b8728d8c06057e782c8b
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2021-02-23 16:50:59 -0300

    ✅Add integration test for save action node
    Co-authored-by: Lucas Haug <LucasHaug@users.noreply.github.com>
    Co-authored-by: Lucas Schneider <lucastrschneider@users.noreply.github.com>

--- thundervolt/behavior_trees/nodes/action/save.py
+++ thundervolt/behavior_trees/nodes/action/save.py
@@ -5,31 +5,36 @@ from ..execution_node import ExecutionNode
 from thundervolt.core import data
 from thundervolt.actions.line_action import LineAction
 
+GOAL_LINE_X = data.FIELD_LENGTH/2 - data.ROBOT_SIZE*0.75
+GOAL_LINE_Y = data.GOAL_WIDTH/2 - data.ROBOT_SIZE/4
+
 class SaveGoal(ExecutionNode):
-    def __init__(self, name, role, field_data, team_command):
+    def __init__(self, name, role, field_data, team_command, save_time):
         super().__init__(name, role, field_data)
         self.team_command = team_command
+        self.save_time = save_time
 
     def setup(self):
         self.action = LineAction(
                         kp_ang=8.0, ki_ang=0.005, kd_ang=2.0, tolerance_ang=0.03,
-                        kp_lin=200.0, ki_lin=0.03, kd_lin=3.0, tolerance_lin=0.005,
+                        kp_lin=200.0, ki_lin=0.03, kd_lin=3.0, tolerance_lin=0.01,
                         saturation_ang=(8*np.pi/3), integral_fade_ang=0.75,
                         saturation_lin=(200*0.2), integral_fade_lin=0.75,
                         linear_decay_std_dev=np.pi/30)
 
     def initialise(self):
-        self.action.initialize(self.parameters.robot_id, (-0.70, -0.18), (-0.70, 0.18))
+        self.action.initialize(self.parameters.robot_id, (-GOAL_LINE_X, -GOAL_LINE_Y), (-GOAL_LINE_X, GOAL_LINE_Y))
 
     def update(self):
         ball_position = (self.field_data.ball.position.x, self.field_data.ball.position.y)
         ball_velocity = (self.field_data.ball.velocity.x, self.field_data.ball.velocity.y)
-        ball_distance = -(ball_position[0] + data.FIELD_LENGTH/2)
+        ball_distance = -(ball_position[0] + GOAL_LINE_X - data.ROBOT_SIZE/2)
 
         if ball_velocity[0] < 0:
             ball_time = ball_distance / ball_velocity[0]
-            if ball_time < 0.5:
-                goal_point = np.clip(ball_time * ball_velocity[1], -data.FIELD_WIDTH/2, data.FIELD_WIDTH/2)
+            if ball_time < self.save_time:
+                goal_point = np.zeros(2)
+                goal_point[1] = ball_position[1] + ball_time * ball_velocity[1]
                 self.action.set_goal(goal_point)
 
                 robot_cmd, action_status = self.action.update(self.field_data)
