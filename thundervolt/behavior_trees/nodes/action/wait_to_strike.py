5c5f98a8d8fdd4953e6782c89ade537e4629efc6
lucas-pvg <lucaspvg1@gmail.com>
2021-02-24 15:43:57 -0300

    :bug: Updated WaitToStrike node

--- thundervolt/behavior_trees/nodes/action/wait_to_strike.py
+++ thundervolt/behavior_trees/nodes/action/wait_to_strike.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.defchararray import multiply
 import py_trees
 
 from ..execution_node import ExecutionNode
@@ -7,29 +8,30 @@ from thundervolt.actions.follow_field_action import FollowFieldAction
 from thundervolt.vector_fields import fields, combinations, plotter
 
 class WaitToStrike(ExecutionNode):
-    def __init__(self, name, role, field_data, team_command):
+    def __init__(self, name, role, field_data, team_command, x_partition):
         super().__init__(name, role, field_data)
         self.team_command = team_command
+        self.x_partition = x_partition
 
 
     def setup(self):
         self.action = FollowFieldAction(
                         kp_ang=7.0, ki_ang=0.005, kd_ang=2.0,
                         kp_lin=200.0, ki_lin=0.03, kd_lin=3.0, tolerance_lin=0.005,
-                        saturation_ang=(15*np.pi), integral_fade_ang=0.75,
-                        saturation_lin=(200), integral_fade_lin=0.75,
+                        saturation_ang=(8*np.pi), integral_fade_ang=0.75,
+                        saturation_lin=(200 * 0.8), integral_fade_lin=0.75,
                         base_speed=40, linear_decay_std_dev=np.pi/4)
 
 
     def initialise(self):
-        self.division_field = fields.LineField(
-            target = (self.field_data.ball.position.x, 0),
+        division_field = fields.LineField(
+            target = (self.x_partition, 0),
             theta = np.pi / 2,
             size = data.FIELD_WIDTH / 2,
             side = 'positive',
             repelling = True,
-            max_dist = data.ROBOT_SIZE,
-            multiplier = 1.5
+            max_dist = data.ROBOT_SIZE/2,
+            multiplier = 1
         )
 
         repell_field = combinations.ObstaclesField(
@@ -38,9 +40,21 @@ class WaitToStrike(ExecutionNode):
             multiplier = 0.8
         )
 
+        wall_field = combinations.WallField(
+            max_dist = data.ROBOT_SIZE * 2/3,
+            multiplier = 0.7
+        )
+
+        self.attracting_field = fields.RadialField(
+            target = (self.x_partition, self.field_data.ball.position.y + 2 * data.ROBOT_SIZE),
+            multiplier = 0.8
+        )
+
         self.vector_field = fields.VectorField()
-        self.vector_field.add(self.division_field)
+        self.vector_field.add(division_field)
         self.vector_field.add(repell_field)
+        self.vector_field.add(wall_field)
+        self.vector_field.add(self.attracting_field)
         self.action.initialize(self.parameters.robot_id, self.vector_field)
 
 
@@ -48,19 +62,14 @@ class WaitToStrike(ExecutionNode):
         player_y = self.field_data.robots[self.parameters.robot_id].position.y
         ball_y = self.field_data.ball.position.y
 
-        if ball_y - data.ROBOT_SIZE < player_y < ball_y + data.ROBOT_SIZE:
-            if (data.FIELD_WIDTH/2 + ball_y - data.ROBOT_SIZE) <= (data.FIELD_WIDTH/2 - ball_y + data.ROBOT_SIZE):
-                goal_y = ball_y + data.ROBOT_SIZE
-            else:
-                goal_y = ball_y - data.ROBOT_SIZE
+        if (data.FIELD_WIDTH/2 + ball_y - 2 * data.ROBOT_SIZE) <= (data.FIELD_WIDTH/2 - ball_y + 2 * data.ROBOT_SIZE):
+            goal_y = ball_y + 2 * data.ROBOT_SIZE
         else:
-            goal_y = player_y
+            goal_y = ball_y - 2 * data.ROBOT_SIZE
 
-        goal = (self.field_data.ball.position.x + data.ROBOT_SIZE, goal_y)
-
+        goal = (self.x_partition + data.ROBOT_SIZE, goal_y)
+        self.attracting_field.target = goal
         self.vector_field.update(self.field_data, self.parameters.robot_id)
-        self.division_field.target = (self.field_data.ball.position.x, 0)
-        self.action.set_goal(np.array(goal))
 
         robot_cmd, action_status = self.action.update(self.field_data)
         self.team_command.commands[self.parameters.robot_id] = robot_cmd
@@ -78,5 +87,5 @@ class WaitToStrike(ExecutionNode):
     def plot_field(self):
         self.vector_field.update(self.field_data, self.parameters.robot_id)
         my_plotter = plotter.FieldPlotter('Wait to Strike')
-        my_plotter.plot(self.division_field)
+        my_plotter.plot(self.attracting_field)
         my_plotter.plot(self.vector_field)
