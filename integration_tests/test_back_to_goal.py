bc9e692d2c5b537bee4530fd904748e5f021fe27
LucasHaug <lucas.haug@hotmail.com>
2021-02-23 21:54:58 -0300

    ✨ Add back to goal and ✅ test

---
+++ integration_tests/test_back_to_goal.py
@@ -0,0 +1,47 @@
+import test_base  # pylint: disable=import-error
+import numpy as np
+import py_trees
+
+from thundervolt.comm.vision import FiraVision
+from thundervolt.comm.control import FiraControl
+from thundervolt.core.data import FieldData
+from thundervolt.core.command import TeamCommand
+from thundervolt.behavior_trees.nodes.action.back_to_goal import BackToGoal
+
+
+def main():
+    team_color_yellow = False
+
+    field_data = FieldData()
+    team_command = TeamCommand()
+
+    vision = FiraVision(team_color_yellow, field_data)
+    blue_control = FiraControl(team_color_yellow, team_command)
+
+    blue_control.stop_team()
+
+    bb_client = py_trees.blackboard.Client()
+    bb_client.register_key(key="/goalkeeper/robot_id", access=py_trees.common.Access.WRITE)
+    bb_client.goalkeeper.robot_id = 1
+
+    my_tree = BackToGoal("Test Node", "/goalkeeper", field_data, team_command)
+
+    my_tree.setup()
+
+    try:
+        while True:
+            vision.update()
+            my_tree.tick_once()
+            blue_control.update()
+            
+            if my_tree.status == py_trees.common.Status.SUCCESS:
+                print("Reach goal!")
+                break
+
+    except KeyboardInterrupt:
+        pass
+
+    blue_control.stop_team()
+
+if __name__ == '__main__':
+    main()
---
+++ thundervolt/behavior_trees/nodes/action/back_to_goal.py
@@ -0,0 +1,84 @@
+import numpy as np
+import py_trees
+
+from ..execution_node import ExecutionNode
+from thundervolt.core import data
+from thundervolt.actions.follow_field_action import FollowFieldAction
+from thundervolt.vector_fields.fields import VectorField, RadialField
+from thundervolt.vector_fields.combinations import ObstaclesField
+
+from thundervolt.vector_fields.plotter import FieldPlotter
+
+
+class BackToGoal(ExecutionNode):
+    def __init__(self, name, role, field_data, team_command):
+
+        """
+        Action node to go back to goal
+        Args:
+            name (str): Behaviour name.
+            role (str): Robot role namespace for black board client.
+            field_data (FieldData): Field information to be stored in the node.
+            team_command (TeamCommand): Angular speed commands for the robots (rad/s).
+        """
+
+        super().__init__(name, role, field_data)
+        self.team_command = team_command
+        self.goal_x_position = -(data.FIELD_LENGTH/2 - data.ROBOT_SIZE * 0.75)
+
+    def setup(self):
+        self.vector_field = VectorField(name="Back to Goal!")
+
+        repelling_field = ObstaclesField(
+            max_radius=0.2,
+            decay_radius=.1,
+            multiplier=0.9,
+        )
+
+        attracting_field = RadialField(
+            target = (self.goal_x_position, 0),
+            max_radius = 3.0,
+            decay_radius = 0.3,
+            repelling = False,
+        )
+
+        self.ball_repelling_field = RadialField(
+            target = (self.field_data.ball.position.x, self.field_data.ball.position.y),
+            max_radius = 0.4,
+            decay_radius = 0.05,
+            repelling = True,
+        )
+
+        self.vector_field.add(attracting_field)
+        self.vector_field.add(repelling_field)
+        self.vector_field.add(self.ball_repelling_field)
+
+        self.action = FollowFieldAction(
+                        kp_ang=8.0, ki_ang=0.005, kd_ang=2.0,
+                        # kp_lin=200.0, ki_lin=0.03, kd_lin=3.0, tolerance_lin=0.005,
+                        saturation_ang=(8*np.pi/3), integral_fade_ang=0.75,
+                        # saturation_lin=(200*0.2), integral_fade_lin=0.75,
+                        base_speed=100, linear_decay_std_dev=np.pi/4)
+
+    def initialise(self):
+        self.action.initialize(self.parameters.robot_id, self.vector_field)
+
+    def update(self):
+        self.vector_field.update(self.field_data, self.parameters.robot_id)
+        self.ball_repelling_field.target = (self.field_data.ball.position.x, self.field_data.ball.position.y)
+
+        robot_cmd, action_status = self.action.update(self.field_data)
+        self.team_command.commands[self.parameters.robot_id] = robot_cmd
+
+        if action_status:
+            return py_trees.common.Status.SUCCESS
+        else:
+            return py_trees.common.Status.RUNNING
+
+    def terminate(self, new_status):
+        pass
+
+    def plot_field(self):
+        self.vector_field.update(self.field_data, self.parameters.robot_id)
+        my_plotter = FieldPlotter('Back to goal field!')
+        my_plotter.plot(self.vector_field)
