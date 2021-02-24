ff05ea20ff456d0b78c2a1d180397db996df35a5
lucas-pvg <lucaspvg1@gmail.com>
2021-02-24 15:12:22 -0300

    :bug: Updated WaitToGetBall parameters and goal

--- thundervolt/behavior_trees/nodes/action/wait_to_get_ball.py
+++ thundervolt/behavior_trees/nodes/action/wait_to_get_ball.py
@@ -15,10 +15,11 @@ class WaitToGetBall(ExecutionNode):
 
     def setup(self):
         self.action = FollowFieldAction(
-                        kp_ang=8.0, ki_ang=0.001, kd_ang=3.0, kp_lin=350.0,
-                        ki_lin=0.001, kd_lin=2.0, tolerance_lin=0.005, saturation_ang=(3*np.pi/6),
-                        max_integral_ang=np.pi/20, integral_fade_ang=0.75, max_integral_lin=1.0,
-                        integral_fade_lin=0.75, linear_decay_std_dev=np.pi/30)
+                        kp_ang=7.0, ki_ang=0.005, kd_ang=2.0,
+                        kp_lin=200.0, ki_lin=0.03, kd_lin=3.0, tolerance_lin=0.005,
+                        saturation_ang=(6*np.pi), integral_fade_ang=0.75,
+                        saturation_lin=(200*0.6), integral_fade_lin=0.75,
+                        base_speed=40, linear_decay_std_dev=np.pi/4)
 
 
     def initialise(self):
@@ -28,42 +29,47 @@ class WaitToGetBall(ExecutionNode):
             size = data.FIELD_WIDTH / 2,
             side = 'positive',
             repelling = True,
-            max_dist = data.ROBOT_SIZE,
-            multiplier = 1.5
+            max_dist = data.ROBOT_SIZE/2,
+            multiplier = 1
         )
 
         self.repell_field = combinations.ObstaclesField(
             max_radius = 0.3,
             decay_radius = 0.05,
-            multiplier = 0.5
+            multiplier = 0.7
+        )
+
+        self.wall_field = combinations.WallField(
+            max_dist = data.ROBOT_SIZE * 2/3,
+            multiplier = 0.7
         )
 
         self.target_field = fields.OrientedAttractingField(
             target = (self.field_data.ball.position.x + data.ROBOT_SIZE, self.field_data.ball.position.y),
             direction = (1,0),
-            nodes_radius = data.ROBOT_SIZE * 3/2,
-            multiplier = 0.2
+            nodes_radius = data.ROBOT_SIZE,
+            multiplier = 0.6
         )
 
         self.vector_field = fields.VectorField()
         self.vector_field.add(self.division_field)
         self.vector_field.add(self.repell_field)
+        self.vector_field.add(self.wall_field)
+        self.vector_field.add(self.target_field)
+
         self.action.initialize(self.parameters.robot_id, self.vector_field)
 
 
     def update(self):
         ball_x = self.field_data.ball.position.x
 
-        if ball_x < self.x_partition:
-            goal = (ball_x + data.ROBOT_SIZE, self.field_data.robots[self.parameters.robot_id].position.y)
+        if ball_x - data.ROBOT_SIZE * 3/2 < self.x_partition:
+            goal = (self.x_partition + data.ROBOT_SIZE, self.field_data.robots[self.parameters.robot_id].position.y)
         else:
-            goal = (ball_x - data.ROBOT_SIZE, self.field_data.ball.position.y)
-            self.vector_field.add(self.target_field)
-
-        self.vector_field.update(self.field_data, self.parameters.robot_id)
+            goal = (ball_x - data.ROBOT_SIZE * 3/2, self.field_data.ball.position.y)
+            
         self.target_field.target = goal
-        self.action.set_goal(np.array(goal))
-
+        self.vector_field.update(self.field_data, self.parameters.robot_id)
         robot_cmd, action_status = self.action.update(self.field_data)
         self.team_command.commands[self.parameters.robot_id] = robot_cmd
 
@@ -79,6 +85,4 @@ class WaitToGetBall(ExecutionNode):
     def plot_field(self):
         self.vector_field.update(self.field_data, self.parameters.robot_id)
         my_plotter = plotter.FieldPlotter('Wait to Get Ball Plot')
-        my_plotter.plot(self.division_field)
-        my_plotter.plot(self.target_field)
         my_plotter.plot(self.vector_field)
