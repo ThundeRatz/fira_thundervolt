44d5b2bee1d4d868dc132b19de551816d2ccf0e7
lucastrschneider <lucastrschneider@gmail.com>
2021-02-25 12:46:21 -0300

    ✨ Add defend corner node -  Update go near corner

--- thundervolt/behavior_trees/nodes/action/go_near_corner.py
+++ thundervolt/behavior_trees/nodes/action/go_near_corner.py
@@ -7,7 +7,7 @@ from thundervolt.vector_fields import fields, combinations
 from thundervolt.actions.follow_field_action import FollowFieldAction
 
 class GoNearCorner(ExecutionNode):
-    def __init__(self, name, role, field_data, team_command, y_position=-0.45):
+    def __init__(self, name, role, field_data, team_command, y_position = 0.45, limit_dir = -0.5):
 
         """
         Create an action node to make the robot go near the corner kick area
@@ -16,28 +16,28 @@ class GoNearCorner(ExecutionNode):
             role (string): role of the player (/goalkeeper, /defender or /attacker)
             field_data (FieldData): information received from the field (e.g.: position of each player and ball)
             team_command (TeamCommand): velocity commands for a robot
-            y_position (float, optional): y goal. Defaults to -0.45
+            y_position (float, optional): initial y position
         """
 
         super().__init__(name, role, field_data)
-        if y_position < 0:
-            self.goal_y = np.clip(y_position, -data.FIELD_WIDTH/2, -data.GOAL_AREA_WIDTH/2)
-        else:
-            self.goal_y = np.clip(y_position, data.GOAL_AREA_WIDTH/2, data.FIELD_WIDTH/2)
+        self.goal_y = np.clip(abs(y_position), data.GOAL_AREA_WIDTH/2, data.FIELD_WIDTH/2)
 
         self.goal_x = -(data.FIELD_LENGTH/2 - data.GOAL_AREA_DEPTH)  # Initial value
         self.team_command = team_command
+
+        self.limit_dir = limit_dir
+
+        self.parameters.register_key('defend_upper', access=py_trees.common.Access.WRITE)
 
     def setup(self):
 
         self.action = FollowFieldAction(
                         kp_ang=7.0, ki_ang=0.005, kd_ang=2.0,
-                        kp_lin=50.0, ki_lin=0.01, kd_lin=3.0, tolerance_lin=0.05,
+                        kp_lin=50.0, ki_lin=0.01, kd_lin=3.0, tolerance_lin=0.1,
                         saturation_ang=(8*np.pi/3), integral_fade_ang=0.75,
                         # saturation_lin=(200*0.2), integral_fade_lin=0.75,
                         base_speed=40, linear_decay_std_dev=np.pi/4)
 
-    def initialise(self):
         # Field declarations
         self.attract_field = fields.RadialField(
             target = (self.goal_x, self.goal_y),
@@ -63,14 +63,31 @@ class GoNearCorner(ExecutionNode):
         self.vector_field.add(self.attract_field)
         self.vector_field.add(area_field)
 
+
+    def initialise(self):
+        if self.field_data.ball.position.y > 0.0:
+            upper = True
+            self.goal_y = abs(self.goal_y)
+        else:
+            upper = False
+            self.goal_y = -abs(self.goal_y)
+
+        self.parameters.defend_upper = upper
+
         self.action.initialize(self.parameters.robot_id, self.vector_field)
 
+
     def update(self):
-        self.vector_field.update(self.field_data, self.parameters.robot_id)
-        if self.field_data.ball.position.x < -data.FIELD_LENGTH/3:
-            self.goal_x = self.field_data.ball.position.x
+
+        # if self.field_data.ball.position.x < -data.FIELD_LENGTH/3:
+        #     self.goal_x = self.field_data.ball.position.x
+
+        self.goal_x = np.clip(self.field_data.ball.position.x, -data.FIELD_LENGTH/2, self.limit_dir)
+
         self.action.set_goal(np.array([self.goal_x, self.goal_y]))
+
         self.attract_field.target = (self.goal_x, self.goal_y)
+        self.vector_field.update(self.field_data, self.parameters.robot_id)
 
         robot_cmd, action_status = self.action.update(self.field_data)
         self.team_command.commands[self.parameters.robot_id] = robot_cmd
