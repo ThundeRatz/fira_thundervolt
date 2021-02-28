32d5830468c8e68ad0d8e67dba3345a80706280e
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2021-02-28 15:02:44 -0300

    ⚡️ Change init state

--- thundervolt/coach.py
+++ thundervolt/coach.py
@@ -30,7 +30,7 @@ class Coach(object):
 
 
     def initialise(self):
-        """
+         """
         Calculate robots roles when the game starts
         """
 
@@ -40,16 +40,16 @@ class Coach(object):
         closer_to_ball = self._ordered_closest_robots_to_point(ball_pos)
         closer_to_goal = self._ordered_closest_robots_to_point(goal_pos)
 
-        self.striker_id = closer_to_ball[0]
+        self.goalkeeper_id = closer_to_goal[0]
 
-        if closer_to_goal[0] != self.striker_id:
-            self.goalkeeper_id = closer_to_goal[0]
+        if closer_to_ball[0] != self.goalkeeper_id:
+            self.striker_id = closer_to_ball[0]
         else:
-            self.goalkeeper_id = closer_to_goal[1]
+            self.striker_id = closer_to_ball[1]
 
-        self.defender_id = closer_to_ball[1]
-        if self.defender_id == self.goalkeeper_id:
-            self.defender_id = closer_to_ball[2]
+        self.defender_id = closer_to_goal[1]
+        if self.defender_id == self.striker_id:
+            self.defender_id = closer_to_goal[2]
 
         self.bb_client.goalkeeper.robot_id = self.goalkeeper_id
         self.bb_client.defender.robot_id = self.defender_id
